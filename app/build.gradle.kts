dbb9dfa7e909e78845e3bdc63ab501b8bb08b133
广陵动物园园长 <129580824+jt230902@users.noreply.github.com>
2026-08-02 22:28:25 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-core")
+implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.animation:animation")
     implementation("androidx.documentfile:documentfile:1.0.1")
 }
